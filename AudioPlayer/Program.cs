45e03cf14481e6c05933947b6d624fc960136b0b
se-yoshi <se.yoshi@gmail.com>
2018-09-17 13:25:05 +0900

    音声再生処理の実装

--- AudioPlayer/Program.cs
+++ AudioPlayer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace AudioPlayer
 {
@@ -6,7 +10,35 @@ namespace AudioPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var files = args.SelectMany(arg => File.Exists(arg) ? new[] { arg }
+                : Directory.Exists(arg) ? Directory.EnumerateFiles(arg, "*", SearchOption.AllDirectories)
+                : throw new FileNotFoundException(arg));
+
+            var lockObject = new object();
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    Console.WriteLine(file);
+
+                    using (var reader = new AudioFileReader(file))
+                    using (var output = new WasapiOut())
+                    {
+                        lock (lockObject)
+                        {
+                            output.Init(reader);
+                            output.PlaybackStopped += (s, e) => { lock (lockObject) { Monitor.PulseAll(lockObject); } };
+                            output.Play();
+                            Monitor.Wait(lockObject);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+            }
         }
     }
 }
